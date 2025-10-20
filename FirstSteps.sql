2274e866ada8a596eba1ac1aaec09b51551f88f8
uayebcaballero <uayeb.caballero@gmail.com>
2025-10-19 19:36:08 -0600

    more queries

--- FirstSteps.sql
+++ FirstSteps.sql
@@ -8,7 +8,27 @@ create table academics.students(
     , email nvarchar(100) not null UNIQUE
 )
 
+
+--Modificar la estructura de la tabla agregando una nueva columna 
+alter table academics.students add age int null
+
+
 select * from academics.students
+
+
+--- Ejemplo de inserción exitosa
+INSERT into academics.students(
+    firstname
+    , lastname
+    , idperson
+    , email
+) values (
+    'Uayeb'
+    , 'Caballero'
+    , '111'
+    , 'uayeb.caballero@gmail.com'
+)
+
 
 INSERT into academics.students(
     firstname
@@ -16,8 +36,37 @@ INSERT into academics.students(
     , idperson
     , email
 ) values (
-    'John'
-    , 'Doe'
+    'Uayeb'
+    , 'Caballero'
     , '111'
-    , 'john2.doe@example.com'
+    , 'uayeb.caballero@gmail.com'
 )
+
+
+--- Ejemplo de actualizacion exitosa
+UPDATE academics.students
+SET firstname = 'Uayeb', lastname = 'Rodriguez', email = 'uayeb.caballero@gmail.com', idperson= '111'
+WHERE id = 3
+
+-- ejemplo para actualizar todos los registros
+update academics.students
+set age = 19
+
+-- actualizar el "age" a 36, donde el lastname es igual a Caballero
+UPDATE academics.students
+set age = 36
+where lastname = 'Caballero'
+
+-- ejemplo para eliminiar un registro con id = 3
+delete FROM academics.students
+where id = 3
+
+
+--- selecionar a todos los estudiantes menores de 20 años
+select
+    id 
+    , lastname
+    , email
+    , age
+from academics.students
+where age < 20
